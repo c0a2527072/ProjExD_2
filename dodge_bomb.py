cb8b53fb74b95682c2e0a6cf54704f875dcb2333
saito rio <c0a2527072@edu.teu.ac.jp>
2026-04-21 14:24:32 +0900

    爆弾表示

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import random
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -20,6 +22,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img=pg.Surface((20,20))  #爆弾用の空のsurface
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0,WIDTH), random.randint(0,HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,6 +54,8 @@ def main():
                 sum_mv[1] += mv[1]  
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct)  # 爆弾を表示させる
         pg.display.update()
         tmr += 1
         clock.tick(50)
